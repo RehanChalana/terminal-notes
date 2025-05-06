4a078cc9e7f60903c54437b3f1153c380bb71101
Rehan Chalana <rehanchalanaprsnl@gmail.com>
2025-05-06 10:33:52 +0530

    Sync notes: Tuesday 06 May 2025 10:33:52 AM IST

--- note.sh
+++ note.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NOTES_DIR="./notes"
-CONFIG_FILE="./c"
+CONFIG_FILE="./config"
 
 # -p ensures that script does not throws an error if directory already exists.
 # it also ensures that parent directories are created if they don't already exist.
@@ -26,5 +26,50 @@ show_help() {
     echo "  config <editor>     Set default editor (e.g., vim, nano, micro)"
 }
 
-show_help
+# $1 is the first command line argument passed to the shell script
+case "$1" in 
+    new)
+        # -z returns true if string is empty
+        [[ -z "$2" ]] && echo "Note name required." && exit 1
+        mkdir -p "$(dirname "$NOTES_DIR/$2")"
+        $EDITOR "$NOTES_DIR/$2.md"
+        ;;
+    list)
+        # sed command is removing the $NOTES_DIR prefix from the results 
+        # So we get only relative paths and file names in the result
+        find "$NOTES_DIR" -type f -name "*.md" | sed "s|$NOTES_DIR/||"
+        ;;
+    delete)
+        [[ -z "$2" ]] && echo "Note name required." && exit 1
+        NOTE_PATH="$NOTES_DIR/$2.md"
+        if [[ -f "$NOTE_PATH" ]]; then
+            rm "$NOTE_PATH"
+            echo "Deleted $2"
+        else 
+            echo "Note not found."
+        fi
+        ;;
+    search)
+        [[ -z "$2" ]] && echo "Keyword required." && exit 1
+        # -rn --> recursive and include line number
+        grep -rn --color=always "$2" "$NOTES_DIR"
+        ;;
+    sync)
+        if [[ -z "$GITHUB_TOKEN" || -z "$GITHUB_REPO" ]]; then
+            echo "Github token or repo not set in the config"
+            exit 1
+        fi
+
+        git pull "https://$GITHUB_TOKEN@github.com/$GITHUB_REPO.git"
+        git add .
+        git commit -m "Sync notes: $(date)" || echo "No changes to commit"
+        git push "https://$GITHUB_TOKEN@github.com/$GITHUB_REPO.git"
+        ;;
+    help)
+        show_help
+        ;;
+esac
+
+
+
 
